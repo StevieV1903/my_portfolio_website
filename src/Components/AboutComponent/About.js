6dbd3849bf71ec6821768e1de72d17669669fd6a
Steve Vance <steve.vance@hotmail.co.uk>
2020-08-20 12:28:34 +0100

    about component complete and styled

--- src/Components/AboutComponent/About.js
+++ src/Components/AboutComponent/About.js
@@ -1,13 +1,40 @@
 import React from "react";
 import './About.css'
+import headshot from "../../Images/headshot_BW.jpg"
 
 const About = () => {
 
     return (
-        <div className="about-title"> 
-            <h1>About Me</h1>
+        <>
+        <div className="about-container">
+          <img
+            className="about-headshot"
+            src={headshot}
+            alt="headshot of steve vance"
+          />
+            <div className="about-text">
+                <h1>
+                Hi, I'm Steve Vance
+                </h1>
+                <p>
+                I'm a Software Developer currently living in Dunbar, East Lothian and I have recently graduated from <a href="https://codeclan.com/">
+                Codeclan</a>.
+                </p>
+                <p>
+                I am a client centred Software Developer motivated to deliver software solutions that truly exceed customer expectations. With excellent communication skills I enjoy working collaboratively or equally well independently. 
+                </p>
+                <p> 
+                Seeking a role in the tech sector, I am enthusiastic, supportive, empathetic, loyal and driven to improve and I would love the opportunity to put this new technical skillset into practice within a dynamic company that encourages further learning and supported development.
+                </p>
+                <p>
+                In my spare time, I love to play chess, cricket and table tennis. I also enjoy walking, cycling and cooking pizza, alfresco. 
+                </p>
+
+            
+          </div>
         </div>
-    )
-}
+    </>
+  );
+};
 
 export default About;
--- src/Components/HomePageComponent/HomePage.js
+++ src/Components/HomePageComponent/HomePage.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import './HomePage.css'
 
 const HomePage = () => {
 
+    useEffect(()=> {
+        window.scrollTo( 0, 0 )
+    });
+
     return (
-        <div className="homepage-title">
-            <h1>HomePage</h1>
-        </div>
+        <>
+            <header>
+                <div className="homepage-title">
+                    <h1>Hello World!</h1>
+                    <h2>I'm Steve...</h2>
+                    <h3>Software Developer.</h3>
+                </div>
+            </header>
+
+            <div>
+            <Link to="/Projects">
+            <div className="hompage-body-link">
+            {/* <img src={foodndrink} alt="food and drink" /> */}
+            <h1 className="homepage-body-title"> Projects</h1>
+            </div>
+            </Link>
+          </div>
+        </>
     )
 }
 
