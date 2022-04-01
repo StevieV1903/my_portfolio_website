c4a4d94ae50664beb081de70cc7f619c553a5fdf
Steve Vance <steve.vance@hotmail.co.uk>
2022-04-01 15:02:07 +0100

    refactored the navbar component

--- src/Components/NavBarComponent/NavBar.js
+++ src/Components/NavBarComponent/NavBar.js
@@ -20,128 +20,43 @@ const NavBar = () => {
                 <img src={ svLogo }></img>
             </div>
 
-            <ul className="navlist"> 
-
-                <li>
-                    <Link to="/" className="navlink-icon">
-                    <FontAwesomeIcon icon={faHome} />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/" className="navlink-text">
-                    <span>home</span>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/About" className="navlink-icon">
-                    <FontAwesomeIcon icon={faUser} />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/About" className="navlink-text">
-                    <span>about</span>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/Projects" className="navlink-icon">
-                    <FontAwesomeIcon icon={faFolderOpen} />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/Projects" className="navlink-text">
-                    <span>projects</span>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/Contact" className="navlink-icon">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to="/Contact" className="navlink-text">
-                    <span>contact</span>
-                    </Link>
-                </li>
-
+        
+            <div className="navlist"> 
                 
-
-                {/* <li>
-                    <Link to="/" className="navlink">
+                <Link to="/" className="navlink-icon">
                     <FontAwesomeIcon icon={faHome} />
-                    <span>  home</span>
-                    </Link>
-                </li> */}
+                </Link>
+                <Link to="/" className="navlink-text">
+                    home
+                </Link>
 
-                {/* <li>
-                    <Link to="/About" className="navlink">
+                <Link to="/about" className="navlink-icon">
                     <FontAwesomeIcon icon={faUser} />
-                    <span>  about</span>
-                    </Link> 
-                </li> */}
+                </Link>
+                <Link to="/about" className="navlink-text">
+                    about
+                </Link>
 
-                {/* <li><Link to="/Projects" className="navlink">
-                    <FontAwesomeIcon icon={faFolderOpen} />
-                    <span>  projects</span>
-                    </Link>
-                </li> */}
-
-                {/* <li>
-                    <Link to="/Contact" className="navlink">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    <span>  contact</span>
-                    </Link>
-                </li> */}
-
-            </ul>
+                <Link to="/Projects" className="navlink-icon">
+                <FontAwesomeIcon icon={faFolderOpen} />
+                </Link>
+                <Link to="/Projects" className="navlink-text">
+                    projects
+                </Link>
+                
+                <Link to="/Contact" className="navlink-icon">
+                <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
+                <Link to="/Contact" className="navlink-text">
+                contact
+                </Link>
+                
+            </div>
 
         </nav>
 
 
-    //    <nav className="navbar">
-            
-    //         <div className="title">
-    //             <h2>StevieV1903</h2>
-    //         </div>
-
-    //         <ul className="navlist"> 
-
-    //             <li>
-    //                 <Link to="/">
-    //                 <FontAwesomeIcon icon={faHome} />
-    //                 Home
-    //                 </Link>
-    //             </li>
-
-    //             <li>
-    //                 <Link to="/About">
-    //                 <FontAwesomeIcon icon={faUser} />
-    //                     About
-    //                 </Link> 
-    //             </li>
-
-    //             <li><Link to="/Projects">
-    //             <FontAwesomeIcon icon={faFolderOpen} />
-    //                 Projects
-    //                 </Link>
-    //             </li>
-
-    //             <li>
-    //                 <Link to="/Contact">
-    //                 <FontAwesomeIcon icon={faEnvelope} />
-    //                     Contact
-    //                 </Link>
-    //             </li>
-
-    //         </ul>
-
-    //     </nav>
+    
     )
 
 
